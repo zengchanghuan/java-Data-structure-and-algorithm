5cfbcbfc1bc9d64b09b0e2a6c13e3821b07c4ddc
zengchanghuan <zengchanghuan@gmail.com>
2021-05-18 16:30:11 +0800

    优化fibonacci

--- Fibonacci/src/cn/com/jryaodian/Fibonacci.java
+++ Fibonacci/src/cn/com/jryaodian/Fibonacci.java
@@ -2,7 +2,7 @@ package cn.com.jryaodian;
 
 public class Fibonacci {
 
-	public static int fib(int n) {
+	public static int fibonacci(int n) {
 		if (n <= 1)
 		{
 			return n;
@@ -11,10 +11,27 @@ public class Fibonacci {
 			return fib(n - 1) + fib(n - 1);
 		}
 	}
+	public static int fibonacci2(int n) {
+		if (n <= 1)
+		{
+			return n;
+		}
+		else {
+			int first = 0;
+			int second = 1;
+			for (int i = 0;i < n - 1;i++) {
+				int sum = first + second;
+				first = second;
+				second = sum;
+				
+			}
+			return second;
+		}
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(fib(30));
-
+//		System.out.println(fibonacci(30));
+		System.out.println(fibonacci2(64));
 	}
 
 }
